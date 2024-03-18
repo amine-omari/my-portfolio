9840f410b915e9b7d347afc48223da01c1287afa
amine alaoui <omari1amineco@gmail.com>
2024-03-18 11:26:04 +0000

    fix: Github icon width and height

--- src/icons/social/Github.jsx
+++ src/icons/social/Github.jsx
@@ -4,8 +4,8 @@ const Github = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="30"
-      height="30"
+      width="25"
+      height="25"
       viewBox="0 0 1024 1024"
       fill="none"
     >
