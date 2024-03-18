b88af41c212330b710055dbe157ec025d052bec5
amine alaoui <omari1amineco@gmail.com>
2024-03-18 13:22:54 +0000

    feat: add usePathName to NavLink component

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,6 +1,13 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavLink = ({ link }) => {
+  const pathName = usePathname();
+
+  console.log(pathName);
+
   return <Link href={link.url}>{link.title}</Link>;
 };
 
