d6d3a3594278a19b9e2367f39ca815504db9ff07
amine alaoui <omari1amineco@gmail.com>
2024-03-18 13:17:16 +0000

    feat: add a link to NavLink component

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,5 +1,7 @@
-const NavLink = () => {
-  return <div>NavLink</div>;
+import Link from "next/link";
+
+const NavLink = ({ link }) => {
+  return <Link href={link.url}>{link.title}</Link>;
 };
 
 export default NavLink;
