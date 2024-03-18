4fd7b197e807505633aae4b5a5e5620284314853
amine alaoui <omari1amineco@gmail.com>
2024-03-18 11:14:53 +0000

    feat: ad links items in md screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,13 +28,21 @@ const Navbar = () => {
 
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="hidden md:flex space-x-4">
+        {links.map((link) => (
+          <Link key={link.title} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
       {/* LOGO */}
-      <div>
+      <div className="md:hidden">
         <Link href="/">
           <Logo />
         </Link>
       </div>
-      <div>
+      {/* Menu Container */}
+      <div className="md:hidden">
         {/* Menu Button */}
         <button
           className="w-10 h-8 flex flex-col justify-between relative z-50"
