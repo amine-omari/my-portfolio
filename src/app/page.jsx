03b13cb18ad6d60e900e54edd3d5b2d80ae2c2e6
amine alaoui <omari1amineco@gmail.com>
2024-03-23 18:39:29 +0000

    feat: add buttons to text container

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,20 +9,18 @@ const Homepage = () => {
       </div>
 
       {/* Text Container */}
-      <div className="h-1/2">
+      <div className="h-1/2 flex flex-col space-y-8 items-center justify-center">
         {/* Title */}
-        <h1>Amine Omari CREATIVE DEVELOPER</h1>
+        <h1 className="text-4xl font-bold">Amine Omari</h1>
 
         {/* Description */}
         <p>
-          Hello! I'm a frontend developer. My job is to make websites that
-          people love to use. I'm really good at coding with HTML, CSS, and
-          JavaScript. I also know how to use special tools like React.js and
-          Next.js to make websites even better. I like to add cool animations to
-          websites using GSAP to make them more interesting. I'm very careful
-          about the small details and always aim for top quality. I'm excited to
-          work with you to bring your ideas to life and make your website stand
-          out in the online world. Let's team up to make something awesome!
+          Hi👋! I'm a frontend developer. I create websites people enjoy using.
+          I'm skilled in HTML, CSS, and JavaScript, and I use tools like
+          React.js and Next.js to enhance websites. I add fun animations using
+          GSAP to make them more appealing. I pay close attention to detail and
+          strive for excellence. Let's collaborate to bring your ideas to life
+          and make your website shine online!
         </p>
       </div>
     </div>
