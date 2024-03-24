e7bcc0d2e258bbb07ed24b6a36caebffb6035489
amine alaoui <omari1amineco@gmail.com>
2024-03-24 17:34:52 +0000

    feat: add animation to Contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,17 @@
+"use client";
+import { motion } from "framer-motion";
+
 const PortfolioPage = () => {
-  return <div>page</div>;
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      page
+    </motion.div>
+  );
 };
 
 export default PortfolioPage;
