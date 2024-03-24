e5ec7cc14da8bfca02c8b8d81ab837f1680ff94b
amine alaoui <omari1amineco@gmail.com>
2024-03-24 17:24:56 +0000

    feat: add a pathName animation

--- src/components/TransitionProvider.jsx
+++ src/components/TransitionProvider.jsx
@@ -17,12 +17,21 @@ const TransitionProvider = ({ children }) => {
           className="w-screen h-screen fixed bg-black rounded-b-[100px] z-40"
           animate={{ height: "0vh" }}
           exit={{ height: "140vh" }}
-          transition={{ duration: 2, ease: "easeOut" }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
         />
+        <motion.div
+          className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50 h-fit w-fit"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          {pathName.substring(1)}
+        </motion.div>
         <motion.div
           className="w-screen h-screen fixed bg-black rounded-t-[100px] bottom-0 z-40"
           initial={{ height: "140vh" }}
-          animate={{ height: "0vh", transition: { delay: 2 } }}
+          animate={{ height: "0vh", transition: { delay: 0.5 } }}
         />
         <div className="h-24">
           <Navbar />
