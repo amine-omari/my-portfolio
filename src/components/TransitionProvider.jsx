f834274aa636dc084c813af437ea601fc79aeaf9
amine alaoui <omari1amineco@gmail.com>
2024-03-24 16:57:48 +0000

    feat: add a usePathName to TransitionProvider component

--- src/components/TransitionProvider.jsx
+++ src/components/TransitionProvider.jsx
@@ -2,11 +2,17 @@
 import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import Navbar from "./Navbar";
+import { usePathname } from "next/navigation";
 
 const TransitionProvider = ({ children }) => {
+  const pathName = usePathname();
+
   return (
     <AnimatePresence>
-      <div className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100">
+      <div
+        key={pathName}
+        className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100"
+      >
         <motion.div
           className="w-screen h-screen fixed bg-black rounded-b-[100px] z-40"
           animate={{ height: "0vh" }}
