f743e89b0e97d4e83f9b0dc775fdf0bfb111ba68
amine alaoui <omari1amineco@gmail.com>
2024-03-24 17:06:50 +0000

    feat: add animate container

--- src/components/TransitionProvider.jsx
+++ src/components/TransitionProvider.jsx
@@ -8,7 +8,7 @@ const TransitionProvider = ({ children }) => {
   const pathName = usePathname();
 
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
       <div
         key={pathName}
         className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100"
@@ -16,8 +16,13 @@ const TransitionProvider = ({ children }) => {
         <motion.div
           className="w-screen h-screen fixed bg-black rounded-b-[100px] z-40"
           animate={{ height: "0vh" }}
-          exit={{ height: "100vh" }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
+          exit={{ height: "140vh" }}
+          transition={{ duration: 2, ease: "easeOut" }}
+        />
+        <motion.div
+          className="w-screen h-screen fixed bg-black rounded-t-[100px] bottom-0 z-40"
+          initial={{ height: "140vh" }}
+          animate={{ height: "0vh", transition: { delay: 2 } }}
         />
         <div className="h-24">
           <Navbar />
