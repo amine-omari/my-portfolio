7ec0eb546a294afd131a8187c7ced38475553f53
amine alaoui <omari1amineco@gmail.com>
2024-04-10 19:15:06 +0000

    fix: Skill hover suration style in Skill component

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Skill = () => {
+const Skill = ({ label }) => {
   return (
-    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:text-black hover:bg-white">
-      Skill
+    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:text-black hover:bg-white duration-200">
+      {label}
     </div>
   );
 };
