3959a31eb5821b81fb19183c0ba5732075b2232d
amine alaoui <omari1amineco@gmail.com>
2024-04-10 19:38:47 +0000

    fix: Skill component style

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Skill = ({ label }) => {
   return (
-    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:text-black hover:bg-white duration-200">
+    <div className="rounded py-[7px] px-2 text-sm cursor-pointer bg-black text-white hover:text-black hover:bg-white duration-200">
       {label}
     </div>
   );
