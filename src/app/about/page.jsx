90f1d23d5861f5d9d370214b9aa7e8ec5835d669
amine alaoui <omari1amineco@gmail.com>
2024-04-10 18:05:39 +0000

    feat: add Biography content to about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -14,10 +14,36 @@ const AboutPage = () => {
         {/* Text Container */}
         <div>
           {/* Biography Container */}
+          <div className="flex flex-col gap-12 justify-center">
+            {/* Biography Title */}
+            <h1>BIOGRAPHY</h1>
+
+            {/* Biography Description */}
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat
+              nesciunt expedita asperiores veritatis sapiente maiores illum
+              aspernatur animi fugit et? Qui, numquam quae facilis nobis culpa
+              sequi ea sint, rem praesentium quos, hic nesciunt voluptatum et
+              necessitatibus saepe quas temporibus at aperiam est facere? Cum
+              aliquam laborum adipisci eum inventore?
+            </p>
+            {/* Biography Quote */}
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+              quae.
+            </span>
+
+            {/* Biography Signature */}
+            <div></div>
+
+            {/* Scroll Icon SVG */}
+            <div></div>
+          </div>
+
+          {/* Skills Container */}
           <div></div>
-          {/* Biography Container */}
-          <div></div>
-          {/* Biography Container */}
+
+          {/* Experience Container */}
           <div></div>
         </div>
         {/* SVG Container */}
