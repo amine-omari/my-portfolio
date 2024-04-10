e2f8ab4d7739cb24f010ca8696e281a1e347ee20
amine alaoui <omari1amineco@gmail.com>
2024-04-10 19:02:58 +0000

    feat: add Skill component style

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Skill = () => {
-  return <div>Skill</div>;
+  return (
+    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:text-black hover:bg-white">
+      Skill
+    </div>
+  );
 };
 
 export default Skill;
