df37e18c54d3fd547e6863dc4be786f995f8e85c
amine alaoui <omari1amineco@gmail.com>
2024-04-11 14:45:22 +0000

    feat: add content to Skils component

--- src/app/about/Skills.jsx
+++ src/app/about/Skills.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="flex flex-col space-y-12 justify-center">
+      {/* Skills Title */}
+      <h1 className="font-bold text-2xl">SKILLS</h1>
+
+      {/* Skills List*/}
+      <div className="flex flex-wrap max-w-sm gap-2">
+        {SKILLS.map(({ id, label }) => (
+          <Skill key={id} label={label} />
+        ))}
+      </div>
+
+      {/* Scroll Icon SVG */}
+      <div>
+        <button className="hover:scale-110 hover:text-[#EBB361] duration-200">
+          <CircleArrow />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
