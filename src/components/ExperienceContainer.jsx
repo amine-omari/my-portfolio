fb67a78ef85916f7f61774500be572be9a856bd1
amine alaoui <omari1amineco@gmail.com>
2024-04-11 21:04:24 +0000

    feat: add content style in ExperienceContainer component

--- src/components/ExperienceContainer.jsx
+++ src/components/ExperienceContainer.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const ExperienceContainer = () => {
   return (
     <div className="flex max-w-xs justify-between">
-      <div className="mr-10 md:mr-20">
-        <h2>Title</h2>
-        <p>Description</p>
-        <time>2024-Now</time>
-        <p>Company</p>
+      <div className="mr-10 md:mr-20 space-y-4">
+        <h2 className="bg-white p-3 font-semibold rounded-b-lg rounded-l-lg">
+          Title
+        </h2>
+        <p className="text-sm">Description</p>
+        <p className="text-sm italic">2024-Now</p>
+        <p className="bg-white rounded p-1.5">Company</p>
       </div>
       <div className="flex flex-col items-center">
         <div className="bg-white border-2 border-red-500 min-h-[24px] w-6 rounded-full" />
