eda1737140f9c9aab481570643de28594e466fe6
amine alaoui <omari1amineco@gmail.com>
2024-04-11 21:20:50 +0000

    fix: TransitionProvider component

--- src/components/TransitionProvider.jsx
+++ src/components/TransitionProvider.jsx
@@ -36,7 +36,7 @@ const TransitionProvider = ({ children }) => {
         <div className="h-24">
           <Navbar />
         </div>
-        <div className="h-[calc(100vh-6rem)] my-10 md:my-20">{children}</div>
+        <div className="h-[calc(100vh-6rem)]">{children}</div>
       </div>
     </AnimatePresence>
   );
