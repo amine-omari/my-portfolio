27b341c0bf7a2298af91e968dfe62d5b0f2fba81
amine alaoui <omari1amineco@gmail.com>
2024-04-11 19:42:30 +0000

    feat: add line divider to ExperinceContainer component

--- src/components/ExperienceContainer.jsx
+++ src/components/ExperienceContainer.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 const ExperienceContainer = () => {
   return (
-    <div>
-      <h2>Title</h2>
-      <p>Description</p>
-      <time>2024-Now</time>
-      <p>Company</p>
+    <div className="flex max-w-xs justify-between">
+      <div className="mr-10 md:mr-20">
+        <h2>Title</h2>
+        <p>Description</p>
+        <time>2024-Now</time>
+        <p>Company</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="bg-white border-2 border-red-500 min-h-[24px] w-6 rounded-full" />
+        <div className="bg-black h-full w-0.5 rounded-full" />
+      </div>
     </div>
   );
 };
