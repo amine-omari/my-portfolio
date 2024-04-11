fac93e6e18d4ca3467f0c6139edb6a73d27614f0
amine alaoui <omari1amineco@gmail.com>
2024-04-11 14:43:59 +0000

    remove: Skiolls container in about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -61,24 +61,6 @@ const AboutPage = () => {
           <Biography />
 
           {/* Skills Container */}
-          <div className="flex flex-col space-y-12 justify-center">
-            {/* Skills Title */}
-            <h1 className="font-bold text-2xl">SKILLS</h1>
-
-            {/* Skills List*/}
-            <div className="flex flex-wrap max-w-sm gap-2">
-              {SKILLS.map(({ id, label }) => (
-                <Skill key={id} label={label} />
-              ))}
-            </div>
-
-            {/* Scroll Icon SVG */}
-            <div>
-              <button className="hover:scale-110 hover:text-[#EBB361] duration-200">
-                <CircleArrow />
-              </button>
-            </div>
-          </div>
 
           {/* Experience Container */}
           <div></div>
